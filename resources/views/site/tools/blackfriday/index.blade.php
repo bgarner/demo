af93578880d20c2f6aa9825f8a41c48cd8927f74
Brent Garner <bgarner@gmail.com>
2016-12-19 08:24:01 -0700

    timestamp

--- resources/views/site/tools/blackfriday/index.blade.php
+++ resources/views/site/tools/blackfriday/index.blade.php
@@ -70,7 +70,7 @@
 {{--                     <table class="table table-striped table-bordered table-hover dataTable addSelector" id="" style="display: none;" aria-describedby="DataTables_Table_0_info" role="grid">
                     </table> --}}
 
-                        <p>Last Updated: Friday, December 16, 2016 - 11:09 AM MST</p>
+                        <p>Last Updated: Monday, December 19, 2016 - 8:23 AM MST</p>
 
                     <table class="table table-bordered table-hover" id="">
                         <thead>
