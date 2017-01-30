c2e0cdc2ad4856d3e663b9e0f5de4729e83fc6b2
Brent Garner <bgarner@gmail.com>
2017-01-30 16:03:07 -0700

    making the deeplink work

---
+++ public/deeplink/deeplink.js
@@ -0,0 +1,26 @@
+$( document ).ready(function() {
+
+    localStorage.removeItem('userStoreNumber');
+    localStorage.removeItem('userStoreName');
+
+    var urlParams = new URLSearchParams(window.location.search);
+    var storeNum = urlParams.get('store');
+
+    var store = (function () {
+        $.ajax({
+            'async': false,
+            'global': false,
+            'url': STORE_API_DOMAIN + "/store/" + storeNum,
+            'dataType': "json",
+            'success': function (data) {
+                store = data;
+            }
+        }).done(function(){
+            if(Object.keys(store).length > 0){
+                localStorage.setItem('userStoreNumber', store.store_number);
+                localStorage.setItem('userStoreName', store.store_number + " " + store.name);
+            }
+            window.location = "/";            
+        });
+    })(); 
+});
