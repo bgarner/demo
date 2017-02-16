f4d55385a422dfd76992ca44acb2fd61744dbf97
Brent Garner <bgarner@gmail.com>
2017-02-16 14:39:19 -0700

    tiny tiny circles for the store timelines

--- resources/views/manager/storeprofile.blade.php
+++ resources/views/manager/storeprofile.blade.php
@@ -21,6 +21,20 @@
     <link rel="stylesheet" type="text/css" media="screen" href="/css/animate.css">
     <link rel="stylesheet" type="text/css" media="screen" href="/css/app.css">
     <link rel="stylesheet" type="text/css" media="screen" href="/css/skins/manager/skin.css">
+
+    <style>
+    .circle{
+        border-radius: 50%;
+        width: 15px;
+        height: 15px;
+        font-size: 8px;
+        text-align: center;
+        padding: 0px;
+        margin: 3px;
+        display: inline-block;
+        color: #fff;
+    }
+    </style>
 </head>
 
 <body>
@@ -89,25 +103,7 @@
 
         </nav>
         </div>
-            <div class="row wrapper border-bottom white-bg page-heading">
-                <div class="col-lg-10">
-                    <h2>Profile</h2>
-                    <ol class="breadcrumb">
-                        <li>
-                            <a href="index.html">Home</a>
-                        </li>
-                        <li>
-                            <a>Extra Pages</a>
-                        </li>
-                        <li class="active">
-                            <strong>Profile</strong>
-                        </li>
-                    </ol>
-                </div>
-                <div class="col-lg-2">
 
-                </div>
-            </div>
         <div class="wrapper wrapper-content animated fadeInRight">
 
 
@@ -177,7 +173,7 @@
             </div>
             <div class="row">
 
-                <div class="col-lg-3">
+                <div class="col-lg-5 col-md-5">
 
                     <div class="ibox">
                         <div class="ibox-content">
@@ -258,7 +254,7 @@
 
                 </div>
 
-                <div class="col-lg-5">
+                <div class="col-lg-5 col-md-5">
 
                     <div class="social-feed-box">
 
@@ -443,79 +439,9 @@
 
 
                 </div>
-                <div class="col-lg-4 m-b-lg">
-                    <div id="vertical-timeline" class="vertical-container light-timeline no-margins">
-                        <div class="vertical-timeline-block">
-                            <div class="vertical-timeline-icon navy-bg">
-                                <i class="fa fa-briefcase"></i>
-                            </div>
+                <div class="col-lg-2 col-md-2" style="border: thin solid red;">
 
-                            <div class="vertical-timeline-content">
-                                <h2>Meeting</h2>
-                                <p>Conference on the sales results for the previous year. Monica please examine sales trends in marketing and products. Below please find the current status of the sale.
-                                </p>
-                                <a href="#" class="btn btn-sm btn-primary"> More info</a>
-                                    <span class="vertical-date">
-                                        Today <br>
-                                        <small>Dec 24</small>
-                                    </span>
-                            </div>
-                        </div>
-
-                        <div class="vertical-timeline-block">
-                            <div class="vertical-timeline-icon blue-bg">
-                                <i class="fa fa-file-text"></i>
-                            </div>
-
-                            <div class="vertical-timeline-content">
-                                <h2>Send documents to Mike</h2>
-                                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since.</p>
-                                <a href="#" class="btn btn-sm btn-success"> Download document </a>
-                                    <span class="vertical-date">
-                                        Today <br>
-                                        <small>Dec 24</small>
-                                    </span>
-                            </div>
-                        </div>
-
-                        <div class="vertical-timeline-block">
-                            <div class="vertical-timeline-icon lazur-bg">
-                                <i class="fa fa-coffee"></i>
-                            </div>
-
-                            <div class="vertical-timeline-content">
-                                <h2>Coffee Break</h2>
-                                <p>Go to shop and find some products. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's. </p>
-                                <a href="#" class="btn btn-sm btn-info">Read more</a>
-                                <span class="vertical-date"> Yesterday <br><small>Dec 23</small></span>
-                            </div>
-                        </div>
-
-                        <div class="vertical-timeline-block">
-                            <div class="vertical-timeline-icon yellow-bg">
-                                <i class="fa fa-phone"></i>
-                            </div>
-
-                            <div class="vertical-timeline-content">
-                                <h2>Phone with Jeronimo</h2>
-                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iusto, optio, dolorum provident rerum aut hic quasi placeat iure tempora laudantium ipsa ad debitis unde? Iste voluptatibus minus veritatis qui ut.</p>
-                                <span class="vertical-date">Yesterday <br><small>Dec 23</small></span>
-                            </div>
-                        </div>
-
-                        <div class="vertical-timeline-block">
-                            <div class="vertical-timeline-icon navy-bg">
-                                <i class="fa fa-comments"></i>
-                            </div>
-
-                            <div class="vertical-timeline-content">
-                                <h2>Chat with Monica and Sandra</h2>
-                                <p>Web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like). </p>
-                                <span class="vertical-date">Yesterday <br><small>Dec 23</small></span>
-                            </div>
-                        </div>
-                    </div>
-
+                    <div class="circle bg-primary"><i class="fa fa-check"></i></div>
                 </div>
 
             </div>
