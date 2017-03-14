06ec8a096bac289bb8821b0fc7a0dbeded2331eb
Brent Garner <bgarner@gmail.com>
2017-03-14 16:51:58 -0600

    seeder classes

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,13 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        $this->call(ComponentsTableSeeder::class);
+        $this->call(ResourcesTableSeeder::class);
+        $this->call(RolesTableSeeder::class);
+        $this->call(RoleResourcesPivotSeeder::class);
+        $this->call(RoleResourcesPivotSeeder::class);
+        $this->call(GroupRolesPivotSeeder::class);
+        $this->call(TaskStatusTypesSeeder::class);
+        $this->call(TaskStoreStatusTypesSeeder::class);
     }
 }
