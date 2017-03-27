6150023a83ac200fd4d92411720db559eee64703
Brent Garner <bgarner@gmail.com>
2017-03-27 10:41:16 -0600

    working ok...

--- resources/views/site/includes/topbar.blade.php
+++ resources/views/site/includes/topbar.blade.php
@@ -117,7 +117,56 @@
 
         <!-- <div class="hidden-xs hidden-sm"> -->
         <div>
-            hello
+
+            <div class="taskbell">
+                <ul class="nav navbar-top-links navbar-right">
+                    <li class="dropdown">
+                                    <a class="dropdown-toggle count-info" data-toggle="dropdown" href="#">
+                                        <i class="fa fa-bell"></i>
+                                        <span class="label label-primary" style="float: left;">8</span>
+                                        <span class="label label-warning" style="float: left; margin-right: 20px;">8</span>
+                                    </a>
+                                    <ul class="dropdown-menu dropdown-alerts">
+                                        <li>
+                                            <a href="mailbox.html">
+                                                <div>
+                                                    <i class="fa fa-envelope fa-fw"></i> You have 16 messages
+                                                    <span class="pull-right text-muted small">4 minutes ago</span>
+                                                </div>
+                                            </a>
+                                        </li>
+                                        <li class="divider"></li>
+                                        <li>
+                                            <a href="profile.html">
+                                                <div>
+                                                    <i class="fa fa-twitter fa-fw"></i> 3 New Followers
+                                                    <span class="pull-right text-muted small">12 minutes ago</span>
+                                                </div>
+                                            </a>
+                                        </li>
+                                        <li class="divider"></li>
+                                        <li>
+                                            <a href="grid_options.html">
+                                                <div>
+                                                    <i class="fa fa-upload fa-fw"></i> Server Rebooted
+                                                    <span class="pull-right text-muted small">4 minutes ago</span>
+                                                </div>
+                                            </a>
+                                        </li>
+                                        <li class="divider"></li>
+                                        <li>
+                                            <div class="text-center link-block">
+                                                <a href="notifications.html">
+                                                    <strong>See All Alerts</strong>
+                                                    <i class="fa fa-angle-right"></i>
+                                                </a>
+                                            </div>
+                                        </li>
+                                    </ul>
+                                </li>
+                            </ul>
+            </div>
+
             <div class="" style="padding: 15px 30px 0px 0px; float:right">
                 <span class="truncate" id="store-name">
                 </span>
