35985cd780969a95d119b33cf0a05867910c4862
Brent Garner <bgarner@gmail.com>
2017-03-28 14:28:08 -0600

    working on the UI view

--- resources/views/site/tasks/index.blade.php
+++ resources/views/site/tasks/index.blade.php
@@ -24,12 +24,12 @@
 
     <div class="wrapper wrapper-content">
         <div class="row">
-            <div class="col-lg-2 col-md-3 col-sm-4 col-xs-4">
+            <!-- <div class="col-lg-2 col-md-3 col-sm-4 col-xs-4">
 
             side bar
 
-            </div>
-            <div class="col-lg-10 col-md-9 col-sm-8 col-xs-8 animated fadeInRight">
+            </div> -->
+            <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 animated fadeInRight">
                 <div class="mail-box-header">
                     <div class="row">
                         <div class="col-md-6">
@@ -39,7 +39,35 @@
 
                 </div>
                 <div class="mail-box">
-
+                    <ul class="todo-list m-t ui-sortable">
+                                            <li>
+                                                <div class="icheckbox_square-green" style="position: relative;"><input type="checkbox" value="" name="" class="i-checks" style="position: absolute; opacity: 0;"><ins class="iCheck-helper" style="position: absolute; top: 0%; left: 0%; display: block; width: 100%; height: 100%; margin: 0px; padding: 0px; background: rgb(255, 255, 255); border: 0px; opacity: 0;"></ins></div>
+                                                <span class="m-l-xs"><strong>Buy a milk</strong></span>
+                                                <small class="label label-primary"><i class="fa fa-clock-o"></i> 1 mins</small>
+                                                <P style="padding-left: 32px; font-size: 12px;line-height: 18px;">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                                                    <ul>
+                                                        <li>sadasdasdasd</li>
+                                                        <li>sadasdasdasd</li>
+                                                        <li>sadasdasdasd</li>
+                                                        <li>sadasdasdasd</li>
+                                                    </ul>
+                                            </li>
+                                            <li>
+                                                <div class="icheckbox_square-green checked" style="position: relative;"><input type="checkbox" value="" name="" class="i-checks" checked="" style="position: absolute; opacity: 0;"><ins class="iCheck-helper" style="position: absolute; top: 0%; left: 0%; display: block; width: 100%; height: 100%; margin: 0px; padding: 0px; background: rgb(255, 255, 255); border: 0px; opacity: 0;"></ins></div>
+                                                <span class="m-l-xs">Go to shop and find some products.</span>
+                                                <small class="label label-info"><i class="fa fa-clock-o"></i> 3 mins</small>
+                                            </li>
+                                            <li>
+                                                <div class="icheckbox_square-green" style="position: relative;"><input type="checkbox" value="" name="" class="i-checks" style="position: absolute; opacity: 0;"><ins class="iCheck-helper" style="position: absolute; top: 0%; left: 0%; display: block; width: 100%; height: 100%; margin: 0px; padding: 0px; background: rgb(255, 255, 255); border: 0px; opacity: 0;"></ins></div>
+                                                <span class="m-l-xs">Send documents to Mike</span>
+                                                <small class="label label-warning"><i class="fa fa-clock-o"></i> 2 mins</small>
+                                            </li>
+                                            <li>
+                                                <div class="icheckbox_square-green" style="position: relative;"><input type="checkbox" value="" name="" class="i-checks" style="position: absolute; opacity: 0;"><ins class="iCheck-helper" style="position: absolute; top: 0%; left: 0%; display: block; width: 100%; height: 100%; margin: 0px; padding: 0px; background: rgb(255, 255, 255); border: 0px; opacity: 0;"></ins></div>
+                                                <span class="m-l-xs">Go to the doctor dr Smith</span>
+                                                <small class="label label-danger"><i class="fa fa-clock-o"></i> 42 mins</small>
+                                            </li>
+                                        </ul>
                 </div>
             </div>
         </div>
