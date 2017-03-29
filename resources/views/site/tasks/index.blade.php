57ae8b6b695d1212d103ffedb6c7af9db734d132
Brent Garner <bgarner@gmail.com>
2017-03-29 14:45:37 -0600

    lots of javascript

--- resources/views/site/tasks/index.blade.php
+++ resources/views/site/tasks/index.blade.php
@@ -43,7 +43,7 @@
                         <li>
                             <a href="#" class="check-link"><i class="fa fa-square-o"></i> </a>
                             <span class="m-l-xs"><strong>Buy a milk</strong></span>
-                            <small class="label label-primary"><i class="fa fa-clock-o"></i> 1 mins</small>
+                            <small class="label label-primary due-date"><i class="fa fa-clock-o due-date-icon"></i>&nbsp;<span class="due-date-text">1 mins</span></small>
                             <P style="padding-left: 32px; font-size: 12px;line-height: 18px;">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
                                 <ul>
                                     <li>sadasdasdasd</li>
@@ -55,18 +55,18 @@
                         <li>
                             <a href="#" class="check-link"><i class="fa fa-square-o"></i> </a>
                             <span class="m-l-xs">Go to shop and find some products.</span>
-                            <small class="label label-info"><i class="fa fa-clock-o"></i> 3 mins</small>
+                            <small class="label label-primary due-date"><i class="fa fa-clock-o"></i> 1 mins</small>
                         </li>
                         <li>
                             <a href="#" class="check-link"><i class="fa fa-square-o"></i> </a>
                             <span class="m-l-xs">Send documents to Mike</span>
-                            <small class="label label-warning"><i class="fa fa-clock-o"></i> 2 mins</small>
+                            <small class="label label-primary due-date"><i class="fa fa-clock-o"></i> 1 mins</small>
                         </li>
                         <li>
 
                             <a href="#" class="check-link"><i class="fa fa-square-o"></i> </a>
                             <span class="m-l-xs">Go to the doctor dr Smith</span>
-                            <small class="label label-danger"><i class="fa fa-clock-o"></i> 42 mins</small>
+                            <small class="label label-primary due-date"><i class="fa fa-clock-o"></i> 1 mins</small>
                         </li>
                     </ul>
 
