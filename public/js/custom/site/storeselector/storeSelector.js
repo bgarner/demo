050d57e954d41d6ee450b51592f70fe61a35f7b3
Brent Garner <bgarner@gmail.com>
2017-04-05 11:53:07 -0600

    fixing drop down for store selector

--- public/js/custom/site/storeselector/storeSelector.js
+++ public/js/custom/site/storeselector/storeSelector.js
@@ -7,7 +7,7 @@ $( document ).ready(function() {
 		resetStore();
 		return;
 	});
-	
+
 	//direct to the dashboard if the localStorage value is set
 	if( !!localStorage.getItem('userStoreNumber') && window.location.pathname == "/" ) {
 		window.location="/" + localStorage.getItem('userStoreNumber');
@@ -16,31 +16,31 @@ $( document ).ready(function() {
 	//redirect to the store chooser if the localStorage value is not set
 	if( localStorage.getItem('userStoreNumber') == null && window.location.pathname != "/" ) {
 		window.location="/";
-	}	
+	}
 
 	//redirect to store as set in localStorage in the event of a change to the URL
 	if( window.location.pathname != "/"  && localStorage.getItem('userStoreNumber') != pathArray[1] )
 	{
 		window.location="/" + localStorage.getItem('userStoreNumber');
-	}	
+	}
 
 	//store selection logic
 	var bannerDropdown = document.getElementById('bannerSelect');
 	var storeDropdown = document.getElementById('storeSelect');
 
-	if(bannerDropdown.length > 0) {
+	if(bannerDropdown) > 0) {
 
 		getBanners();
 
 		bannerDropdown.onchange = function() {
-			localStorage.setItem('userBanner', bannerDropdown.options[bannerDropdown.selectedIndex].value);	
+			localStorage.setItem('userBanner', bannerDropdown.options[bannerDropdown.selectedIndex].value);
 			getStores( localStorage.getItem('userBanner') );
 		}
 		storeDropdown.onchange = function() {
 			localStorage.setItem('userStoreNumber', storeDropdown.options[storeDropdown.selectedIndex].value);
 			localStorage.setItem('userStoreName', storeDropdown.options[storeDropdown.selectedIndex].text);
 			window.location="/" + localStorage.getItem('userStoreNumber');
-		}	
+		}
 	}
 
 });
@@ -48,7 +48,7 @@ $( document ).ready(function() {
 var getBanners = function()
 {
     var jqxhr = $.getJSON( STORE_API_DOMAIN + "/banners", function(json) {
- 
+
     var i=0;
         $.each(json, function(index, element) {
             $("#bannerSelect").append("<option value='"+ element.id +"'>"+ element.name +"</option>");
@@ -59,10 +59,10 @@ var getBanners = function()
 
 var getStores = function(banner)
 {
-	$("#storeSelect").empty();	
+	$("#storeSelect").empty();
 	$("#storeSelect").append("<option></option>");
 	var jqxhr = $.getJSON( STORE_API_DOMAIN + "/banner/" + banner, function(json) {
- 
+
     var i=0;
         $.each(json, function(index, element) {
             $("#storeSelect").append("<option value='"+ element.store_number +"'>"+ element.store_number + " " + element.name +"</option>");
