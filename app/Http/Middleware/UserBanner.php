7858856b7d59e44429cb92347fddb7c87fe8f9f7
Brent Garner <bgarner@gmail.com>
2017-05-12 10:22:42 -0600

    composer update to add debugbar

---
+++ app/Http/Middleware/UserBanner.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Illuminate\Support\Facades\Request as RequestFacade;
+use App\Models\StoreInfo;
+use App\Models\Banner;
+use Closure, Session, Config, App;
+
+class UserBanner
+{
+    public $attributes;
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+
+        $storeNumber = RequestFacade::segment(1);
+        $storeInfo = StoreInfo::getStoreInfoByStoreId($storeNumber);
+        $storeBanner = $storeInfo->banner_id;
+        $banner = Banner::find($storeBanner);
+        
+        Session::flash('banner', $banner);
+        Session::flash('storeInfo', $storeInfo);
+
+        $request->attributes->add(['store_number' => $storeInfo->store_number]);
+        $request->attributes->add(['store_name' => $storeInfo->name]);
+        $request->attributes->add(['store_banner' => $banner->title]);
+        //$request->attributes->add(['banner' => $banner]);
+
+        return $next($request);
+    }
+}
