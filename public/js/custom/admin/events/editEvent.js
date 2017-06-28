8ca383c4aa1e1b52637efedb8b0933507949fcc5
Brent Garner <bgarner@gmail.com>
2017-06-28 11:55:59 -0600

    editing an events all-day status is working

--- public/js/custom/admin/events/editEvent.js
+++ public/js/custom/admin/events/editEvent.js
@@ -2,18 +2,18 @@ $("#allStores").change(function(){
 
 	if ($("#allStores").is(":checked")) {
 
-		$("#storeSelect option").each(function(index){			
+		$("#storeSelect option").each(function(index){
 			$(this).attr('selected', 'selected');
 		});
 		$("#storeSelect").chosen();
-		
+
 	}
 	else if ($("#allStores").not(":checked")) {
 		$("#storeSelect option").each(function(){
 			$(this).removeAttr('selected');
 		});
 		$("#storeSelect").chosen();
-		
+
 	}
 });
 
@@ -27,41 +27,41 @@ $('#attach-selected-folders').on('click', function(){
 
 		console.log('attaching folders');
 		$(".selected-attachments").remove();
-		
+
 		$('input[name^="package_folders"]').each(function(){
-			
+
 			var attr = $(this).attr('data-folderRoot');
 
 			// For some browsers, `attr` is undefined; for others,
 			// `attr` is false.  Check for both.
 			if (typeof attr !== typeof undefined && attr !== false) {
-			    
+
 			    $(".event-attachments-table tbody").append(	'<tr class="selected-attachments"> '+
 													'<td data-attachment-id='+ $(this).val() +'><i class="fa fa-folder-o"></i> '+ $(this).attr("data-foldername") +'</td>'+
 													'<td></td>'+
 													'<td> <a data-attachment-id="'+ $(this).val()+'" id="attachment'+ $(this).val()+'" class="remove-staged-attachment btn btn-danger btn-sm"><i class="fa fa-trash"></i></a></td>'+
 												 '</tr>');
 			}
-			
+
 		});
 	});
 $("body").on('click', ".remove-staged-attachment", function(){
-	
-	
+
+
 	var folder_id = $(this).attr('data-attachment-id');
 	$(this).closest('.selected-attachments').fadeOut(200, function(){
-		$(this).closest('.selected-attachments').remove();	
+		$(this).closest('.selected-attachments').remove();
 	});
-	
+
 
 });
 $(".remove-attachment").on('click', function(){
 	var folder_id = $(this).attr('data-folder-id');
 	$(this).closest(".event-attachments").fadeOut(200, function(){
-		$("#attachments-staged-to-remove").append('<div class="remove_folder" data-folderid='+ folder_id +'>')	
+		$("#attachments-staged-to-remove").append('<div class="remove_folder" data-folderid='+ folder_id +'>')
 	});
-	
-	
+
+
 });
 
 $(".folder-checkbox").on('click', function(){
@@ -78,16 +78,16 @@ $(".folder-checkbox").on('click', function(){
             .find("input[type='checkbox']")
             .prop('checked', false)
             .attr("disabled", false);
-	}	
+	}
 });
 
 $(document).on('click','.event-update',function(){
-  	
+
   	var hasError = false;
 
-  	var eventID = $("#eventID").val(); 
-	var eventBanner = $("#banner").val(); 
-	var eventTitle = $("#title").val(); 
+  	var eventID = $("#eventID").val();
+	var eventBanner = $("#banner").val();
+	var eventTitle = $("#title").val();
     var eventType = $("#event_type").val();
     var eventDescription = CKEDITOR.instances['description'].getData();
     var eventStart = $("#start").val();
@@ -97,37 +97,43 @@ $(document).on('click','.event-update',function(){
 	var attachments = [];
 	var remove_attachments = [];
 
+	if($("#all-day").is(':checked')){
+		var allDay = 1;
+	} else {
+		var allDay = 0;
+	}
+
 	console.log(allStores);
-	
+
 	$(".selected-attachments").each(function(){
 		attachments.push($(this).find('td:first').attr('data-attachment-id'));
 	});
 	$(".remove_folder").each(function(){
 		remove_attachments.push($(this).attr('data-folderid'));
 	});
-	
+
 
     if(eventTitle == '') {
-		swal("Oops!", "This event needs a title.", "error"); 
+		swal("Oops!", "This event needs a title.", "error");
 		hasError = true;
 		$(window).scrollTop(0);
 		return false;
-	}	
+	}
 	if(eventType == ''){
-		swal("Oops!", "Event type missing", "error"); 
+		swal("Oops!", "Event type missing", "error");
 		hasError = true;
 		$(window).scrollTop(0);
 		return false;
 	}
 
     if(eventStart == '' || eventEnd == '') {
-		swal("Oops!", "This event needs a start and end date.", "error"); 
+		swal("Oops!", "This event needs a start and end date.", "error");
 		hasError = true;
 		$(window).scrollTop(0);
 		return false;
-	}	
+	}
 	if( target_stores == null && typeof allStores === 'undefined' ) {
-		swal("Oops!", "Target stores not selected.", "error"); 
+		swal("Oops!", "Target stores not selected.", "error");
 		hasError = true;
 		$(window).scrollTop(0);
 		return false;
@@ -145,6 +151,7 @@ $(document).on('click','.event-update',function(){
 		    	event_type: eventType,
 		    	start: eventStart,
 		    	end: eventEnd,
+				allDay: allDay,				
 		    	target_stores : target_stores,
 		  		allStores : allStores,
 		  		attachments : attachments,
@@ -157,8 +164,8 @@ $(document).on('click','.event-update',function(){
 		        	var errors = data.errors;
 		        	if(errors.hasOwnProperty("title")) {
 		        		$.each(errors.title, function(index){
-		        			$("#title").parent().append('<div class="req">' + errors.title[index]  + '</div>');	
-		        		}); 	
+		        			$("#title").parent().append('<div class="req">' + errors.title[index]  + '</div>');
+		        		});
 		        	}
 		        	if(errors.hasOwnProperty("event_type")) {
 			        	$.each(errors.title, function(index){
@@ -172,25 +179,25 @@ $(document).on('click','.event-update',function(){
 			        }
 			        if(errors.hasOwnProperty("end")) {
 			        	$.each(errors.title, function(index){
-			        		$("#end").parent().parent().append('<div class="req">' + errors.end[0]  + '</div>');	
+			        		$("#end").parent().parent().append('<div class="req">' + errors.end[0]  + '</div>');
 			        	});
 			        }
-			        if(errors.hasOwnProperty("target_stores")) {		        	
+			        if(errors.hasOwnProperty("target_stores")) {
 		        		$("#storeSelect").parent().append('<div class="req">' + errors.target_stores[0]  + '</div>');
 		        	}
-		        	if(errors.hasOwnProperty("allStores")) {		        	
+		        	if(errors.hasOwnProperty("allStores")) {
 		        		$("#storeSelect").parent().append('<div class="req">' + errors.allStores[0]  + '</div>');
 		        	}
 		        }
 		        else{
 		        	swal({title:"Nice!", text: "'" + eventTitle +"' has been updated", type: 'success'}, function(){
 						window.location = '/admin/calendar';
-					});      	
+					});
 		        }
 
-				
+
 		    }
-		});    	
+		});
     }
 
 
