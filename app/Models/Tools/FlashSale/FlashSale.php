87138ce7ecde87be866b245a75ed9935e9bbee98
Brent Garner <bgarner@gmail.com>
2017-07-11 10:05:03 -0600

    model is ready

--- app/Models/Tools/FlashSale/FlashSale.php
+++ app/Models/Tools/FlashSale/FlashSale.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class FlashSale extends Model
 {
-    //
+    public static function getDataByStoreNumber($store_number)
+    {
+    	//strip off the leading zero
+	    $store_number = ltrim($store_number, 'A');
+		$store_number = ltrim($store_number, '0');
+		$data = FlashSale::where('store_number', $store_number)->get();
+    	return $data;
+    }
 }
