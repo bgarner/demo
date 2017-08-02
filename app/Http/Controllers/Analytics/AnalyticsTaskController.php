27e50ba53d8944a5ba170b8fd8001b9bc49bb687
Brent Garner <bgarner@gmail.com>
2017-08-02 15:31:12 -0600

    wip

---
+++ app/Http/Controllers/Analytics/AnalyticsTaskController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\Analytics;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class AnalyticsTaskController extends Controller
+{
+    //
+}
