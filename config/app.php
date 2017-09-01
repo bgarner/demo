e1280c8541e83a7f8dee694b10a4114cdfa4e6b9
Megha Jyoti <jyoti5337@gmail.com>
2017-09-01 10:03:45 -0600

    removing title from dashboard editing

--- config/app.php
+++ config/app.php
@@ -250,6 +250,7 @@ return [
         'App\Http\Controllers\Analytics\AnalyticsAdminController'               => 'Home',
         'App\Http\Controllers\AdminSelectedBannerController'                    => 'Home',
         'App\Http\Controllers\Dashboard\DashboardAdminController'               => 'Dashboard',
+        'App\Http\Controllers\Dashboard\DashboardBackgroundAdminController'     => 'Dashboard',
         'App\Http\Controllers\Document\PackageAdminController'                  => 'Featured Content',
         'App\Http\Controllers\Feature\FeatureAdminController'                   => 'Featured Content',
         'App\Http\Controllers\Feature\FeatureThumbnailAdminController'          => 'Featured Content',
