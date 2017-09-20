2b42bf1d557fcb154ca7f0839e6d061b426a8e35
Brent Garner <bgarner@gmail.com>
2017-09-20 17:24:55 -0600

    Adding better responsiveness to library

--- resources/views/admin/documentmanager/index.blade.php
+++ resources/views/admin/documentmanager/index.blade.php
@@ -33,7 +33,7 @@
 
         <div class="wrapper wrapper-content animated fadeInRight">
             <div class="row">
-                <div class="col-lg-3 col-md-4 col-sm-6">
+                <div class="col-lg-3 col-md-4 col-sm-6 col-xs-6">
                     <div class="ibox float-e-margins">
                         <div class="ibox-content">
                             <div class="file-manager">
@@ -58,16 +58,14 @@
                         </div>
                     </div>
                 </div>
-                <div class="col-lg-9 col-md-8 col-sm-6 animated fadeInRight">
+                <div class="col-lg-9 col-md-8 col-sm-6 col-xs-6 animated fadeInRight">
 
                     <div class="row">
 
-                        <div class="col-lg-12 col-md-12 col-sm-12">
+                        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
 
 
-                            <div id="file-container" class="ibox">
-
-
+                            <div id="file-container" class="" style="height: 100%; width: 100%;">
 
                                 <div class="ibox float-e-margins">
 
