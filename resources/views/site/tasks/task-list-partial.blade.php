7498e4a2b12d1361dd61701456fef89d1a6d8683
Brent Garner <bgarner@gmail.com>
2017-10-20 12:29:57 -0600

    tweeks to layout for tasks

--- resources/views/site/tasks/task-list-partial.blade.php
+++ resources/views/site/tasks/task-list-partial.blade.php
@@ -1,7 +1,7 @@
-<div class="col-lg-2 col-md-3 col-sm-4 col-xs-0">
+<div class="col-lg-4 col-md-4 col-sm-4 col-xs-0">
     @include('site.tasks.tasksidebar')
 </div>
-<div class="col-lg-10 col-md-9 col-sm-8 col-xs-12">
+<div class="col-lg-8 col-md-8 col-sm-8 col-xs-12">
 
     <div class="mail-box-header clearfix">
         <h2>
@@ -18,10 +18,14 @@
             <h4>{{__("Tasks Due Today")}}</h4>
             <ul class="todo-list m-t">
                 @foreach($tasksDueToday as $task)
-                    <li>
-                        <a href="" class="check-link trackclick" data-task-id="{{$task->id}}" data-task-completed="not done"><i class="fa fa-square-o"></i></a>
-                        <span class="m-l-xs"><strong>{{$task->title}} </strong></span>
-                        <small class="label label-danger due-date"><i class="fa fa-clock-o due-date-icon"></i>&nbsp;<span class="due-date-text">{{$task->pretty_due_date}}</span></small>
+                    <li class="due-today-list-item">
+                        <a href="" class="check-link trackclick pull-left" data-task-id="{{$task->id}}" data-task-completed="not done"><i class="fa fa-square-o"></i></a>
+                        <span class="m-l-xs pull-left task-title"><strong>{{$task->title}} </strong></span>
+
+                        <small class="label label-danger due-date pull-right">
+                            <i class="fa fa-clock-o due-date-icon"></i>&nbsp;<span class="due-date-text">{{$task->pretty_due_date}}</span>
+                        </small>
+
                         <div class="task_description">{!! $task->description !!}</div>
                         @if(isset($task->documents))
                         <div class="task_documents">
@@ -41,10 +45,14 @@
             <ul class="todo-list m-t">
                 @foreach($tasksDue as $task)
                     <li>
-                        <a href="" class="check-link trackclick" data-task-id="{{$task->id}}" data-task-completed="not done"><i class="fa fa-square-o"></i></a>
-                        <span class="m-l-xs"><strong>{{$task->title}} </strong></span>
-                        <small class="label label-primary due-date"><i class="fa fa-clock-o due-date-icon"></i>&nbsp;<span class="due-date-text">{{$task->pretty_due_date}}</span></small>
-                        <div class="task_description">{!! $task->description !!}</div>
+                        <a href="" class="check-link trackclick pull-left" data-task-id="{{$task->id}}" data-task-completed="not done"><i class="fa fa-square-o"></i></a>
+                        <span class="m-l-xs pull-left task-title"><strong>{{$task->title}} </strong></span>
+
+                        <div class="label label-primary due-date pull-right">
+                            <i class="fa fa-clock-o due-date-icon"></i>&nbsp;<span class="due-date-text">{{$task->pretty_due_date}}</span>
+                        </div>
+
+                        <div class="task_description clearfix">{!! $task->description !!}</div>
                         @if(isset($task->documents))
                         <div class="task_documents">
 
@@ -63,9 +71,13 @@
             <ul class="todo-list m-t">
                 @foreach($tasksCompleted as $task)
                     <li>
-                        <a href="" class="check-link trackclick" data-task-id="{{$task->id}}" data-task-completed="done"><i class="fa fa-check-square"></i></a>
-                        <span class="todo-completed"><strong>{{$task->title}} </strong></span>
-                        <small class="label label-default due-date"><i class="fa fa-check due-date-icon"></i>&nbsp;<span class="due-date-text">{{$task->pretty_completed_date}}</span></small>
+                        <a href="" class="check-link trackclick pull-left" data-task-id="{{$task->id}}" data-task-completed="done"><i class="fa fa-check-square"></i></a>
+                        <span class="m-l-xs todo-completed pull-left task-title"><strong>{{$task->title}} </strong></span>
+
+                        <small class="label label-default pull-right due-date">
+                            <i class="fa fa-check due-date-icon"></i>&nbsp;<span class="due-date-text">{{$task->pretty_completed_date}}</span>
+                        </small>
+
                         <div class="task_description">{!! $task->description !!}</div>
                         @if(isset($task->documents))
                         <div class="task_documents">
