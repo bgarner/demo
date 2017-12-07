c61aabf70649185093b838202ad60b2ef2898cc1
Brent Garner <bgarner@gmail.com>
2017-12-07 10:24:06 -0700

    turned on the boxing day tracker

--- resources/views/site/includes/sidenav-partials/Tools.blade.php
+++ resources/views/site/includes/sidenav-partials/Tools.blade.php
@@ -5,7 +5,7 @@
                 @endif
                     <a href="#"><i class="fa fa-wrench" aria-hidden="true"></i> <span class="nav-label">{{__($component_label)}}</span><span class="fa arrow"></span></a>
                         <ul class="nav nav-second-level collapse" style="height: 0px;">
-                            {{-- <li><a href="/{{ Request::segment(1) }}/tools/boxingday">Boxing Day Doorcrasher Tracker</a></li> --}}
+                            <li><a href="/{{ Request::segment(1) }}/tools/boxingday">Boxing Day Doorcrasher Tracker</a></li>
                             {{-- <li><a href="/{{ Request::segment(1) }}/tools/bikecount">Bike Count Tracker</a></li> --}}
                             <li><a href="/{{ Request::segment(1) }}/tools/flashsale">DOM Flash Sale Tracker</a></li>
                             <li><a href="/{{ Request::segment(1) }}/tools/fwinitials">Footwear Initials Tracker</a></li>
