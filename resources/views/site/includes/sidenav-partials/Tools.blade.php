06e1029782cd25041b8c63ea7091e901bd6558fc
Megha Jyoti <jyoti5337@gmail.com>
2017-12-12 11:36:42 -0700

    added tracking to tools

--- resources/views/site/includes/sidenav-partials/Tools.blade.php
+++ resources/views/site/includes/sidenav-partials/Tools.blade.php
@@ -1,14 +1,14 @@
 @if (Request::segment(2) == 'tools')
-                <li class="active">
-                @else
-                <li>
-                @endif
-                    <a href="#"><i class="fa fa-wrench" aria-hidden="true"></i> <span class="nav-label">{{__($component_label)}}</span><span class="fa arrow"></span></a>
-                        <ul class="nav nav-second-level collapse" style="height: 0px;">
-                            {{-- <li><a href="/{{ Request::segment(1) }}/tools/boxingday">Boxing Day Doorcrasher Tracker</a></li> --}}
-                            {{-- <li><a href="/{{ Request::segment(1) }}/tools/bikecount">Bike Count Tracker</a></li> --}}
-                            <li><a href="/{{ Request::segment(1) }}/tools/flashsale">DOM Flash Sale Tracker</a></li>
-                            <li><a href="/{{ Request::segment(1) }}/tools/fwinitials">Footwear Initials Tracker</a></li>
-                        </ul>
+<li class="active">
+@else
+<li>
+@endif
+    <a href="#"><i class="fa fa-wrench" aria-hidden="true"></i> <span class="nav-label">{{__($component_label)}}</span><span class="fa arrow"></span></a>
+        <ul class="nav nav-second-level collapse" style="height: 0px;">
+            <li><a href="/{{ Request::segment(1) }}/tools/boxingday" class="trackclick" data-tool-type="doorcrasherTracker">Doorcrasher Tracker</a></li>
+            {{-- <li><a href="/{{ Request::segment(1) }}/tools/bikecount" class="trackclick" data-tool-type="bikecountTracker">Bike Count Tracker</a></li> --}}
+            <li><a href="/{{ Request::segment(1) }}/tools/flashsale" class="trackclick" data-tool-type="flashsaleTracker">DOM Flash Sale Tracker</a></li>
+            <li><a href="/{{ Request::segment(1) }}/tools/fwinitials" class="trackclick" data-tool-type="fwinitialsTracker">Footwear Initials Tracker</a></li>
+        </ul>
 
-                </li>
+</li>
