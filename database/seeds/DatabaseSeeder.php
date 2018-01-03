8709e2ae8cadf36577e446c81a36a47e7277bde2
Brent Garner <bgarner@gmail.com>
2018-01-03 14:38:17 -0700

    updating the whats new

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,35 +11,35 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(GroupsTableSeeder::class);
-        // $this->call(RolesTableSeeder::class);
-        // $this->call(ComponentsTableSeeder::class);
-        // $this->call(ResourceTypeTableSeeder::class);
-        // $this->call(ResourcesTableSeeder::class);
-        // $this->call(GroupRolesPivotSeeder::class);
-        // $this->call(RoleResourcesPivotSeeder::class);
-        // $this->call(RoleComponentPivotSeeder::class);
-        // $this->call(TaskStatusTypesSeeder::class);
-        // $this->call(TaskStoreStatusTypesSeeder::class);
-        // $this->call(VideoTableSeeder::class);
-        // $this->call(PlaylistTableSeeder::class);
-        // $this->call(FeatureCommunicationTypeTableSeeder::class);
-        // $this->call(FeatureTableSeeder::class);
-        // $this->call(StoreComponentTableSeeder::class);
-        // $this->call(AnalyticsAssetTypesTableSeeder::class);
-        // $this->call(StoreTableSeeder::class);
-        // $this->call(DistrictTableSeeder::class);
-        // $this->call(RegionTableSeeder::class);
-        // $this->call(DistrictStorePivotSeeder::class);
-        // $this->call(DistrictRegionPivotSeeder::class);
-        // $this->call(BannerStorePivotSeeder::class);
-        // $this->call(CommunicationTypeBannerTableSeeder::class);
-        // $this->call(FeatureBannerTableSeeder::class);
-        // $this->call(UrgentNoticeBannerTableSeeder::class);
-        // $this->call(BannerTableBannerClassSeeder::class);
-        // $this->call(EventTypeBannerTableSeeder::class);
+        $this->call(GroupsTableSeeder::class);
+        $this->call(RolesTableSeeder::class);
+        $this->call(ComponentsTableSeeder::class);
+        $this->call(ResourceTypeTableSeeder::class);
+        $this->call(ResourcesTableSeeder::class);
+        $this->call(GroupRolesPivotSeeder::class);
+        $this->call(RoleResourcesPivotSeeder::class);
+        $this->call(RoleComponentPivotSeeder::class);
+        $this->call(TaskStatusTypesSeeder::class);
+        $this->call(TaskStoreStatusTypesSeeder::class);
+        $this->call(VideoTableSeeder::class);
+        $this->call(PlaylistTableSeeder::class);
+        $this->call(FeatureCommunicationTypeTableSeeder::class);
+        $this->call(FeatureTableSeeder::class);
+        $this->call(StoreComponentTableSeeder::class);
+        $this->call(AnalyticsAssetTypesTableSeeder::class);
+        $this->call(StoreTableSeeder::class);
+        $this->call(DistrictTableSeeder::class);
+        $this->call(RegionTableSeeder::class);
+        $this->call(DistrictStorePivotSeeder::class);
+        $this->call(DistrictRegionPivotSeeder::class);
+        $this->call(BannerStorePivotSeeder::class);
+        $this->call(CommunicationTypeBannerTableSeeder::class);
+        $this->call(FeatureBannerTableSeeder::class);
+        $this->call(UrgentNoticeBannerTableSeeder::class);
+        $this->call(BannerTableBannerClassSeeder::class);
+        $this->call(EventTypeBannerTableSeeder::class);
 
-        // $this->call(BannerTableBannerClassSeeder::class);
+        $this->call(BannerTableBannerClassSeeder::class);
 
         $this->call(ComponentDeletablePropertySeeder::class);
     }
