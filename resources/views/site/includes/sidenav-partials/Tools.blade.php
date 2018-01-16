2ccf6c2d5b3d1fe3a3bd105623c643e6daae6761
Brent Garner <bgarner@gmail.com>
2018-01-16 10:20:36 -0700

    commenting out the prod deliveries and doorcrasher tracker

--- resources/views/site/includes/sidenav-partials/Tools.blade.php
+++ resources/views/site/includes/sidenav-partials/Tools.blade.php
@@ -5,10 +5,11 @@
 @endif
     <a href="#"><i class="fa fa-wrench" aria-hidden="true"></i> <span class="nav-label">{{__($component_label)}}</span><span class="fa arrow"></span></a>
         <ul class="nav nav-second-level collapse" style="height: 0px;">
-            <li><a href="/{{ Request::segment(1) }}/tools/boxingday" class="trackclick" data-tool-type="doorcrasherTracker">Doorcrasher Tracker</a></li>
+            {{-- <li><a href="/{{ Request::segment(1) }}/tools/boxingday" class="trackclick" data-tool-type="doorcrasherTracker">Doorcrasher Tracker</a></li> --}}
             {{-- <li><a href="/{{ Request::segment(1) }}/tools/bikecount" class="trackclick" data-tool-type="bikecountTracker">Bike Count Tracker</a></li> --}}
             <li><a href="/{{ Request::segment(1) }}/tools/flashsale" class="trackclick" data-tool-type="flashsaleTracker">DOM Flash Sale Tracker</a></li>
 
+            {{-- 
             <li><a href="#"> Product Deliveries <span class="fa arrow"></span></a>
                 <ul class="nav nav-third-level collapse ">
                     <li><a href="/{{ Request::segment(1) }}/tools/fwinitials" class="trackclick" data-tool-type="fwinitialsTracker">Footwear</a></li>
@@ -17,7 +18,7 @@
                     <li><a href="/{{ Request::segment(1) }}/tools/lcinitials" class="trackclick" data-tool-type="lcinitialsTracker">Licensed</a></li>
                 </ul>
             </li>
-            
+            --}}
         </ul>
 
 </li>
