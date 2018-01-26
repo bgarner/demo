6188eb6b97cd8840001a9f62307fc37718d96fb4
Brent Garner <bgarner@gmail.com>
2018-01-26 09:39:00 -0700

    added the initials trackers

--- resources/views/site/includes/sidenav-partials/Tools.blade.php
+++ resources/views/site/includes/sidenav-partials/Tools.blade.php
@@ -9,7 +9,7 @@
             {{-- <li><a href="/{{ Request::segment(1) }}/tools/bikecount" class="trackclick" data-tool-type="bikecountTracker">Bike Count Tracker</a></li> --}}
             <li><a href="/{{ Request::segment(1) }}/tools/flashsale" class="trackclick" data-tool-type="flashsaleTracker">DOM Flash Sale Tracker</a></li>
 
-            {{-- 
+            
             <li><a href="#"> Product Deliveries <span class="fa arrow"></span></a>
                 <ul class="nav nav-third-level collapse ">
                     <li><a href="/{{ Request::segment(1) }}/tools/fwinitials" class="trackclick" data-tool-type="fwinitialsTracker">Footwear</a></li>
@@ -18,7 +18,7 @@
                     <li><a href="/{{ Request::segment(1) }}/tools/lcinitials" class="trackclick" data-tool-type="lcinitialsTracker">Licensed</a></li>
                 </ul>
             </li>
-            --}}
+           
         </ul>
 
 </li>
