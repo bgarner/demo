f645695528cd3665e8b715286651dd680516a2b7
Brent Garner <bgarner@gmail.com>
2018-01-26 13:52:03 -0700

    hide table until loaded to prevent dumping a bunch of json to the screen

--- resources/views/site/tools/hginitials/index.blade.php
+++ resources/views/site/tools/hginitials/index.blade.php
@@ -17,6 +17,10 @@
             cursor: pointer;
         }
 
+        .fwinitialsTable{
+            display: none;
+        }
+
     </style>
 </head>
 
@@ -52,6 +56,11 @@
                         <div class="row">
                             <div class="col-md-12">
                                 <div class="table-responsive clearfix">
+
+                                    <div id="loading">
+                                        <i class="fa fa-spinner fa-spin fa-3x fa-fw"></i><span class="sr-only">Loading..n.</span>
+                                    </div>
+                                                                        
                                     <table class="table table-bordered table-hover fwinitialsTable">
                                         <thead>
                                         <tr>
@@ -145,6 +154,12 @@
              "searching": false
         });
         table.column(9).visible( false );
+
+        $(window).load(function() {
+            // When the page has loaded
+            $('#loading').hide();
+            $(".fwinitialsTable").fadeIn(500);
+        });
         
     </script>
 
