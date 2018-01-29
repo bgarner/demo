c06db09a3e1051624325dd938d249d4c99f8d32a
Brent Garner <bgarner@gmail.com>
2018-01-29 13:31:18 -0700

    migration to add sport/cat drop down to table

--- vendor/composer/autoload_classmap.php
+++ vendor/composer/autoload_classmap.php
@@ -8,6 +8,7 @@ $baseDir = dirname($vendorDir);
 return array(
     'AddAllDayToEventsTable' => $baseDir . '/database/migrations/2017_06_28_152315_add_all_day_to_events_table.php',
     'AddOnOrderCountToBikeTracker' => $baseDir . '/database/migrations/2017_06_23_142132_add_on_order_count_to_bike_tracker.php',
+    'AddSportCatToCommunityDonationsTable' => $baseDir . '/database/migrations/2018_01_29_112304_add_sport_cat_to_community_donations_table.php',
     'AnalyticsAssetTypesTableSeeder' => $baseDir . '/database/seeds/AnalyticsAssetTypesTableSeeder.php',
     'App\\Models\\Alert\\Alert' => $baseDir . '/app/Models/Alert/Alert.php',
     'App\\Models\\Alert\\AlertType' => $baseDir . '/app/Models/Alert/AlertType.php',
