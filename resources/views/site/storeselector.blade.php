deb5e48c160c176ce5577364af417c4361b5205c
Brent Garner <bgarner@gmail.com>
2018-02-05 16:01:32 -0700

    cache busting

--- resources/views/site/storeselector.blade.php
+++ resources/views/site/storeselector.blade.php
@@ -54,7 +54,7 @@
 
 
 
-    <script type="text/javascript" src="/js/env.js"></script>
+    <script type="text/javascript" src="/js/env.js?<?=time();?>"></script>
 	<script type="text/javascript" src="/js/jquery-2.1.1.min.js"></script>
 	<script type="text/javascript" src="/js/custom/site/storeselector/storeSelector.js"></script>
     <script src="/js/bootstrap.min.js"></script>
