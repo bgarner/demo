34f07c0fe50249298307e33ab553d1faea75c36c
Brent Garner <bgarner@gmail.com>
2018-02-22 13:33:08 -0700

    test the mail

--- public/mailtest.php
+++ public/mailtest.php
@@ -1,10 +1,17 @@
 <?php
+$to      = 'brent.garner@fglsports.com';
+$subject = 'the subject';
+$message = 'hello';
+$headers = 'From: FGL Ops Portal<portal@fglsports.com>' . "\r\n" .
+    'Reply-To: no-reply@fglsports.com' . "\r\n" .
+    'X-Mailer: PHP/' . phpversion();
+
 // The message
-$message = "Line 1\r\nLine 2\r\nLine 3";
+$message = "Do not reply to this message\r\n------------------------\r\nLine 1\r\nLine 2\r\nLine 3";
 
 // In case any of our lines are larger than 70 characters, we should use wordwrap()
 $message = wordwrap($message, 70, "\r\n");
 
 // Send
-mail('brent.garner@fglsports.com', 'My Subject', $message);
+mail($to, $subject, $message, $headers);
 ?>
