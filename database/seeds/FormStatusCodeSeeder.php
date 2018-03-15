c3dfba065e3d0aa152e5f00af920dc414c8dffaf
Megha Jyoti <jyoti5337@gmail.com>
2018-03-15 09:02:59 -0600

    updated seeder for status codes

--- database/seeds/FormStatusCodeSeeder.php
+++ database/seeds/FormStatusCodeSeeder.php
@@ -6,11 +6,12 @@ use App\Models\Form\Status;
 class FormStatusCodeSeeder extends Seeder
 {
     private $formStatusCodes = [ 
-    	["store_status" =>	'submitted', 'admin_status' => 'new', 'icon'=>'fa-paper-plane', 'colour' => 'green-bg'], 
+    	["store_status" =>	'submitted', 'admin_status' => 'submitted', 'icon'=>'fa-paper-plane', 'colour' => 'green-bg'], 
     	["store_status" =>	'in progress', 'admin_status' => 'in progress', 'icon'=>'fa-clock-o', 'colour' => 'blue-bg'], 
     	["store_status" =>	'approved', 'admin_status' => 'approved', 'icon'=>'fa-thumbs-o-up', 'colour' => 'green-bg'], 
-    	["store_status" =>	'not approved', 'admin_status' => 'not approved', 'icon'=>'fa-thumbs-o-down', 'colour' => 'green-bg'], 
-    	["store_status" =>	'closed', 'admin_status' => 'closed', 'icon'=>'fa-times-circle', 'colour' => 'green-bg']
+    	["store_status" =>	'not approved', 'admin_status' => 'not approved', 'icon'=>'fa-frown-o', 'colour' => 'red-bg'], 
+    	["store_status" =>	'closed', 'admin_status' => 'closed', 'icon'=>'fa-times-circle', 'colour' => 'black-bg'],
+        ["store_status" =>  'question', 'admin_status' => 'question', 'icon'=>'fa-question', 'colour' => 'red-bg']
 
     ];
     /**
