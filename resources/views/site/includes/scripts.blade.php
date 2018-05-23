9a921d686971830f24c10272dbeb2e3b3a323585
Brent Garner <bgarner@gmail.com>
2018-05-23 12:46:20 -0600

    putting scripts back in the scripts include

--- resources/views/site/includes/scripts.blade.php
+++ resources/views/site/includes/scripts.blade.php
@@ -4,7 +4,9 @@
     <script src="/js/bootstrap.min.js"></script>
     <script src="/js/plugins/metisMenu/jquery.metisMenu.js"></script>
     <script src="/js/plugins/slimscroll/jquery.slimscroll.min.js"></script>
-
+    <script src="/js/plugins/dataTables/datatables.min.js?<?=time();?>"></script>
+    <script src="/js/plugins/videojs/video.js?<?=time();?>"></script>
+    
     <!-- Custom and plugin javascript -->
     <script src="/js/inspinia.js"></script>
     <script src="/js/plugins/pace/pace.min.js"></script>
@@ -15,7 +17,6 @@
 
     <!-- Alerts -->
     <script src="/js/plugins/sweetalert/sweetalert.min.js"></script>
-
     <script src="/js/custom/site/storeselector/storeSelector.js?<?=time();?>"></script>
     <script src="/js/custom/site/launchModal.js?<?=time();?>"></script>
     <script src="/js/custom/trackEvent.js?<?=time();?>"></script>
