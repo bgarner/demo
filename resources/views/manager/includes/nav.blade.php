c906291b13e54f0b04d5ebce1190fd27af409814
Megha Jyoti <jyoti5337@gmail.com>
2018-05-29 13:42:07 -0600

    sidenav for manager

--- resources/views/manager/includes/nav.blade.php
+++ resources/views/manager/includes/nav.blade.php
@@ -1,69 +1,104 @@
-<!-- Dashboard  -->
-@if (Request::is('manager/dashboard') || Request::is('manager/dashboard/*'))
-<li class="active">
-@else
-<li>
-@endif
-    <a href="/manager/dashboard"><i class="fa fa-tachometer"></i> <span class="nav-label">Dashboard</span></a>
-</li>
+<ul class="nav metismenu" id="side-menu">
+
+    <li class="nav-header">
+
+        <div class="dropdown profile-element">
+
+            <span class="text-xs block" style="padding-bottom: 10px; color: #DFE4ED;">
+            Welcome, {{ Auth::user()->firstname }}!
+            <a class="navbar-minimalize minimalize-styl-1 btn btn-primary pull-right" style="padding: 2px 4px; font-size: 8px;" href="#"><i class="fa fa-bars"></i> </a>
+            </span>
+            <a href="{{ url('/logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                <span class="text-xs">
+                    <i class="fa fa-sign-out"></i> Log out
+                </span>
+            </a>
+
+            <br />
 
 
-<!-- Calendar  -->
+            <form id="logout-form" action="{{ url('/logout') }}" method="POST" style="display: none;">
+                {{ csrf_field() }}
+            </form>
 
-@if (  Request::is('manager/calendar/*') || 
-    Request::is('manager/calendar') || 
-    Request::is('manager/productlaunch') || 
-    Request::is('manager/productlaunch/*')
-    ) 
-<li class="active">
-@else
-<li>
-@endif
-    <a href="/manager/calendar"><i class="fa fa-calendar"></i> <span class="nav-label">Calendar</span><span class="fa arrow"></span></a>
-    <ul class="nav nav-second-level collapse">
-        <li><a href="/manager/calendar">Events</a></li>
-        <li><a href="/manager/productlaunch">Product Launches</a></li>
-    </ul>
-</li>
+        </div>
 
+        <div class="logo-element">
+            <a class="navbar-minimalize minimalize-styl-2 btn btn-primary" href="#" style="margin: 1px 5px 10px 15px;"><i class="fa fa-bars"></i></a>
+            <br />
+        </div>
 
-<!-- Communication  -->
+    </li>
 
-
-@if (Request::is('manager/communication/*') || Request::is('manager/communication') )
-<li class="active">
-@else
-<li>
-@endif
-	<a href="/manager/communication"><i class="fa fa-bullhorn"></i> <span class="nav-label">Communications</span></a>
-</li>
+    
+        <!-- Dashboard  -->
+        @if (Request::is('manager/dashboard') || Request::is('manager/dashboard/*'))
+        <li class="active">
+        @else
+        <li>
+        @endif
+            <a href="/manager/dashboard"><i class="fa fa-tachometer"></i> <span class="nav-label">Dashboard</span></a>
+        </li>
 
 
-<!-- Alerts and Notices  -->
+        <!-- Calendar  -->
+
+        @if (  Request::is('manager/calendar/*') || 
+            Request::is('manager/calendar') || 
+            Request::is('manager/productlaunch') || 
+            Request::is('manager/productlaunch/*')
+            ) 
+        <li class="active">
+        @else
+        <li>
+        @endif
+            <a href="/manager/calendar"><i class="fa fa-calendar"></i> <span class="nav-label">Calendar</span><span class="fa arrow"></span></a>
+            <ul class="nav nav-second-level collapse">
+                <li><a href="/manager/calendar">Events</a></li>
+                <li><a href="/manager/productlaunch">Product Launches</a></li>
+            </ul>
+        </li>
 
 
-@if (Request::is('manager/alert/*') || Request::is('manager/alert') || Request::is('manager/urgentnotice') || Request::is('manager/urgentnotice/*')) 
-<li class="active">
-@else
-<li>
-@endif
-    <a href="/manager/alert"><i class="fa fa-exclamation-triangle"></i> <span class="nav-label">Alerts and Notices</span><span class="fa arrow"></span></a>
-    <ul class="nav nav-second-level collapse">
-        <li><a href="/manager/alert">Alerts</a></li>
-        <li><a href="/manager/urgentnotice">Urgent Notices</a></li>
-    </ul>
-</li>
+        <!-- Communication  -->
 
 
-<!-- Tasks  -->
-@if ( Request::is('manager/task/*') || Request::is('manager/task') || Request::is('manager/tasklist') || Request::is('/manager/tasklist/*') ) 
-<li class="active">
-@else
-<li>
-@endif
-     <a href="/manager/task"><i class="fa fa-tasks"></i> <span class="nav-label">Tasks</span><span class="fa arrow"></span></a>
-     <ul class="nav nav-second-level collapse">
-        <li><a href="/manager/tasklist">Task Lists</a></li>
-        <li><a href="/manager/task">Tasks</a></li>
-    </ul>
-</li>
+        @if (Request::is('manager/communication/*') || Request::is('manager/communication') )
+        <li class="active">
+        @else
+        <li>
+        @endif
+        	<a href="/manager/communication"><i class="fa fa-bullhorn"></i> <span class="nav-label">Communications</span></a>
+        </li>
+
+
+        <!-- Alerts and Notices  -->
+
+
+        @if (Request::is('manager/alert/*') || Request::is('manager/alert') || Request::is('manager/urgentnotice') || Request::is('manager/urgentnotice/*')) 
+        <li class="active">
+        @else
+        <li>
+        @endif
+            <a href="/manager/alert"><i class="fa fa-exclamation-triangle"></i> <span class="nav-label">Alerts and Notices</span><span class="fa arrow"></span></a>
+            <ul class="nav nav-second-level collapse">
+                <li><a href="/manager/alert">Alerts</a></li>
+                <li><a href="/manager/urgentnotice">Urgent Notices</a></li>
+            </ul>
+        </li>
+
+
+        <!-- Tasks  -->
+        @if ( Request::is('manager/task/*') || Request::is('manager/task') || Request::is('manager/tasklist') || Request::is('/manager/tasklist/*') ) 
+        <li class="active">
+        @else
+        <li>
+        @endif
+             <a href="/manager/task"><i class="fa fa-tasks"></i> <span class="nav-label">Tasks</span><span class="fa arrow"></span></a>
+             <ul class="nav nav-second-level collapse">
+                <li><a href="/manager/tasklist">Task Lists</a></li>
+                <li><a href="/manager/task">Tasks</a></li>
+            </ul>
+        </li>
+
+</ul>
