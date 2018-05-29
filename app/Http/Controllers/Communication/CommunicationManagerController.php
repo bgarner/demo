6a9987fc91e2a3a6717ecf116449c6f6161dc79d
Megha Jyoti <jyoti5337@gmail.com>
2018-05-29 09:53:16 -0600

    missed one

--- app/Http/Controllers/Communication/CommunicationManagerController.php
+++ app/Http/Controllers/Communication/CommunicationManagerController.php
@@ -32,7 +32,7 @@ class CommunicationManagerController extends Controller
         
         $storeList = StoreInfo::getStoreListingByManagerId($this->user_id);
         $this->stores = array_column($storeList, 'store_number');
-        $this->storeGroups = CustomStoreGroup::getStoreGroupsForManager($this->user_id);
+        $this->storeGroups = CustomStoreGroup::getStoreGroupsForManager($this->stores);
 
         $this->banners = UserBanner::getAllBanners()->pluck( 'id');           
 
