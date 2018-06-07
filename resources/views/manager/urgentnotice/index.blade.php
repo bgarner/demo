71051cbed28ad2809071e9bcfe777d582ebb2166
Megha Jyoti <jyoti5337@gmail.com>
2018-06-07 12:32:53 -0600

    master layout

--- resources/views/manager/urgentnotice/index.blade.php
+++ resources/views/manager/urgentnotice/index.blade.php
@@ -1,94 +1,60 @@
-<!DOCTYPE html>
-<html>
+@extends('manager.layouts.master')
+@section('title', 'Dashboard')
+@section('content')
+    <div class="wrapper wrapper-content">
+        <div class="row">
 
-<head>
-    @section('title', 'Dashboard')
-    <link href="/css/plugins/iCheck/custom.css" rel="stylesheet">
-    @include('manager.includes.head')
+            <div class="col-lg-12 animated fadeInRight">
+                <div class="mail-box-header">
 
-</head>
+                    <h2>
+                        {{__("Urgent Notices")}}
+                    </h2>
 
-<body class="fixed-navigation">
-    <div id="wrapper">
-        <nav class="navbar-default navbar-static-side" role="navigation">
-            <div class="sidebar-collapse">
-              @include('manager.includes.nav')
-            </div>
-        </nav>
-
-        <div id="page-wrapper" class="gray-bg">
-            <div class="row border-bottom">
-                @include('manager.includes.topbar')
-            </div>
-
-
-            <div class="wrapper wrapper-content">
-                <div class="row">
-
-                    <div class="col-lg-12 animated fadeInRight">
-                        <div class="mail-box-header">
-
-                            <h2>
-                                {{__("Urgent Notices")}}
-                            </h2>
-
-                        </div>
-                        <div class="mail-box">
-
-
-                            <table class="table table-hover table-mail">
-                            <tbody>
-                                <thead>
-                                    <tr>
-                                        <th>&nbsp;</th>
-                                        <th> {{__("Title")}}</th>
-                                        <th> {{__("Description")}} </th>
-                                        <th> {{__("Date")}} </th>
-                                    </tr>
-                                </thead>
-
-
-                                @foreach($urgentNotices as $notice)
-                                    <tr>
-                                        <td class="check-mail"><i class="fa fa-bolt"></i></td>
-                                        <td><a class="trackclick" data-urgentnotice-id="{{ $notice->id }}" href="/{{ Request::segment(1) }}/urgentnotice/{{ $notice->id }}">{{ $notice->title }}</td>
-                                        <td>{{ $notice->trunc }}</td>
-                                        <td class="mail-date">{{ $notice->prettyDate }} <small style="font-weight: normal;padding-left: 10px;">({{ $notice->since }} {{__("ago")}})</small></td>
-                                    </tr>
-                                    <tr class=" store-row">
-                                        <td></td>
-                                        <td colspan="3">
-                                            @if( isset($notice->stores) )
-                                            @foreach($notice->stores as $store)
-                                                <span class="badge">{{$store}}</span>
-                                            @endforeach
-                                            
-                                            @elseif( $notice->all_stores == 1 )
-                                                <span class="badge">{{$notice->banner}}</span>
-                                            @endif
-                                        </td>
-                                    </tr>
-                                @endforeach
-                            </tbody>
-                            </table>
-
-
-                        </div>
-                    </div>
                 </div>
-            </div> <!-- class wrapper closes -->
-        </div> <!-- page wrapper -->
-    </div> <!-- wrapper -->
+                <div class="mail-box">
 
 
-    @include('manager.includes.footer')
+                    <table class="table table-mail">
+                    <tbody>
+                        <thead>
+                            <tr>
+                                <th>&nbsp;</th>
+                                <th> {{__("Title")}}</th>
+                                <th> {{__("Description")}} </th>
+                                <th> {{__("Date")}} </th>
+                            </tr>
+                        </thead>
 
-    @include('manager.includes.scripts')
-    <!-- <script src="/js/custom/manager/getArchivedContent.js?<?=time();?>"></script> -->
-    <script src="/js/plugins/iCheck/icheck.min.js"></script>
+
+                        @foreach($urgentNotices as $notice)
+                            <tr>
+                                <td class="check-mail"><i class="fa fa-bolt"></i></td>
+                                <td><a class="trackclick" data-urgentnotice-id="{{ $notice->id }}" href="/{{ Request::segment(1) }}/urgentnotice/{{ $notice->id }}">{{ $notice->title }}</td>
+                                <td>{{ $notice->trunc }}</td>
+                                <td class="mail-date">{{ $notice->prettyDate }} <small style="font-weight: normal;padding-left: 10px;">({{ $notice->since }} {{__("ago")}})</small></td>
+                            </tr>
+                            <tr class=" store-row">
+                                <td></td>
+                                <td colspan="3">
+                                    @if( isset($notice->stores) )
+                                    @foreach($notice->stores as $store)
+                                        <span class="badge">{{$store}}</span>
+                                    @endforeach
+                                    
+                                    @elseif( $notice->all_stores == 1 )
+                                        <span class="badge">{{$notice->banner}}</span>
+                                    @endif
+                                </td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                    </table>
 
 
-    @include('site.includes.modal')
-
-</body>
-</html>
+                </div>
+            </div>
+        </div>
+    </div> <!-- class wrapper closes -->
+    
+@endsection
--- resources/views/manager/urgentnotice/notice.blade.php
+++ resources/views/manager/urgentnotice/notice.blade.php
@@ -1,130 +1,98 @@
-<!DOCTYPE html>
-<html>
+@extends('manager.layouts.master')
+@section('title', 'Dashboard')
 
-<head>
-    @section('title', 'Dashboard')
-    <link href="/css/plugins/iCheck/custom.css" rel="stylesheet">
-    @include('manager.includes.head')
+@section('content')
+<div class="wrapper wrapper-content">
+    <div class="row">
 
-</head>
+        <div class="col-lg-12 animated fadeInRight">
+            <div class="mail-box-header">
+                 <a href="/manager/urgentnotice"><i class="fa fa-chevron-left"></i> {{__("Back")}}</a>
+                <h1>
+                    {{ $notice->title }}
+                     <span class="pull-right font-normal" style="font-size: 16px;">{{ $notice->prettyDate }} <small style="font-weight: normal;padding-left: 10px;">({{ $notice->since }} {{__("ago")}})</small></span>
+                </h1>
 
-<body class="fixed-navigation">
-    <div id="wrapper">
-        <nav class="navbar-default navbar-static-side" role="navigation">
-            <div class="sidebar-collapse">
-              @include('manager.includes.nav')
             </div>
-        </nav>
+            <div class="mail-box">
 
-        <div id="page-wrapper" class="gray-bg">
-            <div class="row border-bottom">
-                @include('manager.includes.topbar')
-            </div>
+                <div class="mail-body">
+                    {!! $notice->description !!}
+                </div>
 
+                @if(count($attached_folders) > 0)
+                <div class="mail-attachment">
+                    <h3>
+                        <span><i class="fa fa-paperclip"></i> {{ count($attached_folders) }} {{__("folders")}}</span>
+                    </h3>
+                    @foreach($attached_folders as $folder)
 
-            <div class="wrapper wrapper-content">
-                <div class="row">
+                            <div class="file-box">
+                                <div class="file">
+                                    <a href="/{{ Request::segment(1) }}/document#!/{{ $folder->global_folder_id }}">
 
-                    <div class="col-lg-12 animated fadeInRight">
-                        <div class="mail-box-header">
-                             <a href="/manager/urgentnotice"><i class="fa fa-chevron-left"></i> {{__("Back")}}</a>
-                            <h1>
-                                {{ $notice->title }}
-                                 <span class="pull-right font-normal" style="font-size: 16px;">{{ $notice->prettyDate }} <small style="font-weight: normal;padding-left: 10px;">({{ $notice->since }} {{__("ago")}})</small></span>
-                            </h1>
-
-                        </div>
-                        <div class="mail-box">
-
-                            <div class="mail-body">
-                                {!! $notice->description !!}
-                            </div>
-
-                            @if(count($attached_folders) > 0)
-                            <div class="mail-attachment">
-                                <h3>
-                                    <span><i class="fa fa-paperclip"></i> {{ count($attached_folders) }} {{__("folders")}}</span>
-                                </h3>
-                                @foreach($attached_folders as $folder)
-
-                                        <div class="file-box">
-                                            <div class="file">
-                                                <a href="/{{ Request::segment(1) }}/document#!/{{ $folder->global_folder_id }}">
-
-                                                    <div class="icon">
-                                                        <i class="fa fa-folder-open"></i>
-                                                    </div>
-
-                                                    <div class="file-name">
-                                                        <div style="font-size: 16px; padding-bottom: 10px;"> {{ $folder->name }}</div>
-
-                                                        <small class="clearfix"><span class="text-muted pull-left">{{ $folder->prettyDate }}</span> <span class="text-muted pull-right">{{ $folder->since }} {{__("ago")}}</span></small>
-                                                    </div>
-                                                </a>
-                                            </div>
-
+                                        <div class="icon">
+                                            <i class="fa fa-folder-open"></i>
                                         </div>
 
-                                @endforeach
-                                <div class="clearfix"></div>
-                            </div>
-                            @endif
-                            @if(count($attached_documents) > 0)
-                           <div class="mail-attachment">
-                                <h3>
-                                    <span><i class="fa fa-paperclip"></i> {{ count($attached_documents) }} {{__("documents")}}</span>
-                                </h3>
+                                        <div class="file-name">
+                                            <div style="font-size: 16px; padding-bottom: 10px;"> {{ $folder->name }}</div>
 
-                                <div class="attachment">
-
-                                    @foreach($attached_documents as $doc)
-
-                                        <div class="file-box">
-                                            <div class="file">
-                                                {!! $doc->anchor_only !!}
-
-                                                    <div class="icon">
-                                                        {!! $doc->icon !!}
-                                                    </div>
-
-
-                                                    <div class="file-name">
-                                                        <div style="font-size: 16px; padding-bottom: 10px;"> {{ $doc->title }}</div>
-
-                                                        <small class="clearfix"><span class="text-muted pull-left">{{ $doc->prettyDate }}</span> <span class="text-muted pull-right">{{ $doc->since }} {{__("ago")}}</span></small>
-
-                                                    </div>
-                                                
-
-                                            </div>
-
+                                            <small class="clearfix"><span class="text-muted pull-left">{{ $folder->prettyDate }}</span> <span class="text-muted pull-right">{{ $folder->since }} {{__("ago")}}</span></small>
                                         </div>
-
-                                    @endforeach
-
-
-                                    <div class="clearfix"></div>
+                                    </a>
                                 </div>
+
                             </div>
-                            @endif
+
+                    @endforeach
+                    <div class="clearfix"></div>
+                </div>
+                @endif
+                @if(count($attached_documents) > 0)
+               <div class="mail-attachment">
+                    <h3>
+                        <span><i class="fa fa-paperclip"></i> {{ count($attached_documents) }} {{__("documents")}}</span>
+                    </h3>
+
+                    <div class="attachment">
+
+                        @foreach($attached_documents as $doc)
+
+                            <div class="file-box">
+                                <div class="file">
+                                    {!! $doc->anchor_only !!}
+
+                                        <div class="icon">
+                                            {!! $doc->icon !!}
+                                        </div>
 
 
-                        </div>
+                                        <div class="file-name">
+                                            <div style="font-size: 16px; padding-bottom: 10px;"> {{ $doc->title }}</div>
+
+                                            <small class="clearfix"><span class="text-muted pull-left">{{ $doc->prettyDate }}</span> <span class="text-muted pull-right">{{ $doc->since }} {{__("ago")}}</span></small>
+
+                                        </div>
+                                    
+
+                                </div>
+
+                            </div>
+
+                        @endforeach
+
+
+                        <div class="clearfix"></div>
                     </div>
                 </div>
-            </div> <!-- class wrapper closes -->
-        </div> <!-- page wrapper -->
-    </div> <!-- wrapper -->
+                @endif
 
 
-    @include('manager.includes.footer')
+            </div>
+        </div>
+    </div>
+</div> <!-- class wrapper closes -->
 
-    @include('manager.includes.scripts')
-    <!-- <script src="/js/custom/manager/getArchivedContent.js?<?=time();?>"></script> -->
-    <script src="/js/plugins/iCheck/icheck.min.js"></script>
-
-
-    @include('site.includes.modal')
-
-</body>
-</html>
+@endsection
+        
