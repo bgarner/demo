c17f6f778fb1bb98b409516f6078dca9850c3a42
Megha Jyoti <jyoti5337@gmail.com>
2018-06-19 16:39:26 -0600

    getting resources for exec user

--- app/Models/StoreApi/StoreInfo.php
+++ app/Models/StoreApi/StoreInfo.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\Request as RequestFacade; 
 use App\Models\Auth\User\UserSelectedBanner;
 use App\Models\Auth\User\UserResource;
+use App\Models\Auth\Role\RoleResource;
+use App\Models\Auth\User\UserRole;
 use App\Models\Auth\Resource\Resource;
 use App\Models\Auth\Resource\ResourceTypes;
 use App\Models\StoreApi\Banner;
@@ -164,10 +166,19 @@ class StoreInfo extends Model
     {
         $storeAPI = env('STORE_API_DOMAIN', false);
 
-        $resource_id = UserResource::where('user_id', $user_id)->first()->resource_id;
+        $userResource = UserResource::where('user_id', $user_id)->first();
 
-        $resource = Resource::find($resource_id);
-        $resource_type_name = ResourceTypes::find($resource->resource_type_id)->resource_name;
+        //user does not have an entry in user_resource_pivot
+        if($userResource) {
+            $resource = Resource::find($userResource->resource_id);
+            $resource_type_name = ResourceTypes::find($resource->resource_type_id)->resource_name;    
+
+        }
+        else{
+            $role_id = UserRole::where('user_id', $user_id)->first()->role_id;
+            $resource_id = RoleResource::where('role_id', $role_id)->first()->resource_type_id;
+            $resource_type_name = ResourceTypes::find($resource_id)->resource_name;    
+        }
 
         switch ($resource_type_name) {
             case 'store':
