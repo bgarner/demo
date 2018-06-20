72f3266a93fcb47269d551511276bf10354a61c5
Brent Garner <bgarner@gmail.com>
2018-06-20 10:43:23 -0600

    wip

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -41,17 +41,16 @@ class DatabaseSeeder extends Seeder
         // $this->call(BannerTableBannerClassSeeder::class);
         // $this->call(ComponentDeletablePropertySeeder::class);
         
+        // $this->call(FormStatusCodeSeeder::class);
+        // $this->call(FormTableSeeder::class);
+        // $this->call(FormGroupAndRolesSeeder::class);
+        // $this->call(FormPermissionTableSeeder::class);
+        // $this->call(BusinessUnitTypesTableSeeder::class);
+        // $this->call(FormTargetTableSeeder::class);
+        // $this->call(UpdateStoreComponentsTableSeeder::class);
 
-        $this->call(FormStatusCodeSeeder::class);
-        $this->call(FormTableSeeder::class);
-        $this->call(FormGroupAndRolesSeeder::class);
-        $this->call(FormPermissionTableSeeder::class);
-        $this->call(BusinessUnitTypesTableSeeder::class);
-        $this->call(FormTargetTableSeeder::class);
-        $this->call(UpdateStoreComponentsTableSeeder::class);
 
-
-        $this->call(UsernameSeeder::class);
+        // $this->call(UsernameSeeder::class);
         
 
     }
