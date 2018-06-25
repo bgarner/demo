d3c203e1576c0fb96183247040a9efd96de0bf82
Brent Garner <bgarner@gmail.com>
2018-06-25 15:27:29 -0600

    all day event for releases

--- app/Models/Event/Event.php
+++ app/Models/Event/Event.php
@@ -136,8 +136,13 @@ class Event extends Model
             $event->event_type_name = EventType::getName($event->event_type);
             $event->background_colour = EventType::getBackground($event->event_type);
             $event->foreground_colour = EventType::getForeground($event->event_type);
-            if( strpos(EventType::getName($event->event_type), "Launch") || strpos(EventType::getName($event->event_type), "launch" ) ){
-                $event->all_day = 1;
+            if( 
+                strpos(EventType::getName($event->event_type), "Launch") || 
+                strpos(EventType::getName($event->event_type), "launch" ) ||
+                strpos(EventType::getName($event->event_type), "Release" ) ||
+                strpos(EventType::getName($event->event_type), "release" ) 
+                ){
+                    $event->all_day = 1;
             }
         }
 
