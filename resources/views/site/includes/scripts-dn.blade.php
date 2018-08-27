567856d056368488cee5f9f103cbf59b41b20e54
Brent Garner <bgarner@gmail.com>
2018-08-27 15:41:49 -0600

    reverting...

--- resources/views/site/includes/scripts-dn.blade.php
+++ resources/views/site/includes/scripts-dn.blade.php
@@ -19,11 +19,11 @@
     <script src="/js/custom/setLang.js?<?=time();?>"></script>
 
     <script type="text/javascript">
-        // $.ajaxSetup({
-        //     headers: {
-        //         'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-        //     }
-        // });
+        $.ajaxSetup({
+            headers: {
+                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+            }
+        });
     </script>
 
     <script type="text/javascript">
