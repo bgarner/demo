b3c0eae80fa1139805b865b5d99d5abed232e860
Brent Garner <bgarner@gmail.com>
2018-09-21 14:14:48 -0600

    add the migration and model

---
+++ app/Models/Form/FormHighlighting.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models\Form;
+
+use Illuminate\Database\Eloquent\Model;
+
+class FormHighlighting extends Model
+{
+    protected $table = 'store_form_highlight';
+    protected $fillable = ['store_number'];
+
+}
