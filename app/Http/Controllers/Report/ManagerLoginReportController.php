f4f6b19cce2d433b487e294eb5d155fd5c5d87aa
Megha Jyoti <jyoti5337@gmail.com>
2018-09-26 11:20:01 -0600

    adding a report view

--- app/Http/Controllers/Report/ManagerLoginReportController.php
+++ app/Http/Controllers/Report/ManagerLoginReportController.php
@@ -4,8 +4,17 @@ namespace App\Http\Controllers\Report;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Models\Auth\User\User;
 
 class ManagerLoginReportController extends Controller
 {
-    //
+    public function index()
+    {
+     	$users = User::getUsersByGroupId(2);
+        return view('manager.report.managerlogin.index')
+        		->with('users', $users);
+            // ->with('toDate', $toDate)
+            // ->with('sinceLastWeek', $sinceLastWeek)
+            // ->with('lastWeek', $lastWeek);
+    }
 }
