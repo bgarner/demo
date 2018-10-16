f045f6212df20f564bb2e2059731b548e817354a
Brent Garner <bgarner@gmail.com>
2018-10-16 16:13:59 -0600

    migration, model, controllers

---
+++ app/Http/Controllers/Surveys/SurveyAdminController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\Surveys;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class SurveyAdminController extends Controller
+{
+    //
+}
---
+++ database/migrations/2018_10_16_150541_create_surveys_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSurveysTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('surveys', function (Blueprint $table) {
+            $table->increments('id');
+            $table->text('name');
+            $table->text('form');
+            $table->timestamp('start');
+            $table->timestamp('end');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('surveys');
+    }
+}
